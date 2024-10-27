8edf0c7d4407bf4dd46f424c2030add194e3a06d
Radamés <radames.idiomas@gmail.com>
2024-10-27 00:17:27 -0300

    Create a products route to deal with products list.

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require('express')
 
 const app = express()
 
+const products = [
+    {
+        name: 'Laptop',
+        price: 400.00,
+        quantity: 4,
+        active: true
+    },
+    {
+        name: 'Keyboard',
+        price: 29.99,
+        quantity: 10,
+        active: true
+    },
+]
+
 app.get('/', (request, response) => {
     response.send('Hello, World!')
+})
+
+app.get('/products', (request, response) => {
+    response.json(products)
 })
 
 app.listen(3000, () => console.log('Server started on port 3000'))
