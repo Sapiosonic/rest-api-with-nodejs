696f2d8b357a2a00195341b8b474593e2980ba96
Radamés <radames.idiomas@gmail.com>
2024-10-27 01:05:19 -0300

    Retrieve a specific product.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const crypto = require('crypto')
 
 const app = express()
 
@@ -6,16 +7,25 @@ app.use(express.json())
 
 const products = [
     {
-        name: 'Laptop',
-        price: 400.00,
-        quantity: 4,
-        active: true
+        "id": "90286eb1-4cfd-42e5-93be-35f448e0f5e6",
+        "name": "Laptop",
+        "price": 400,
+        "quantity": 4,
+        "active": true
     },
     {
-        name: 'Keyboard',
-        price: 29.99,
-        quantity: 10,
-        active: true
+        "id": "90286eb1-4cfd-42e5-93be-35f448e0f5f4",
+        "name": "Keyboard",
+        "price": 29.99,
+        "quantity": 10,
+        "active": true
+    },
+    {
+        "id": "90286eb1-4cfd-42e5-93be-35f448e0f5cf",
+        "name": "Computer",
+        "price": 700,
+        "quantity": 1,
+        "active": true
     },
 ]
 
@@ -24,23 +34,38 @@ app.get('/', (request, response) => {
 })
 
 app.get('/products', (request, response) => {
-    response.json(products)
+    response.status(200).json(products)
 })
 
 app.post('/products', (request, response) => {
     const { name, price, quantity, active } = request.body
 
     if (!name) {
-        return response.sendStatus(422).json({ message: "Name is required!" })
+        return response.status(422).json({ message: "Name is required!" })
     }
+
+    const id = crypto.randomUUID()
+
     products.push({
+        id,
         name,
         price,
         quantity,
         active
     })
 
-    response.sendStatus(201).json({ message: 'Product created successfully!'})
+    response.status(201).json({ message: 'Product created successfully!', id })
+})
+
+app.get('/products/:id', (request, response) => {
+    const product = products.find(product => product.id == request.params.id)
+
+    if (!product) {
+        // return response.status(204).send() 
+        return response.status(404).send({ message: 'Product not found!' }) 
+    }
+
+    response.status(200).json(product)
 })
 
 app.listen(3000, () => console.log('Server started on port 3000'))
