9a49af3219986c47cc3c4ad790076b803fe7af3d
Radamés <radames.idiomas@gmail.com>
2024-10-27 00:10:25 -0300

    Testing API's response

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (request, response) => {
+    response.send('Hello, World!')
+})
+
 app.listen(3000, () => console.log('Server started on port 3000'))
