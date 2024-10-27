7dbe11f2b9120f2410a96520b5944afb842c8083
Radamés <radames.idiomas@gmail.com>
2024-10-27 00:31:15 -0300

    Create a route to test requests.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 const products = [
     {
         name: 'Laptop',
@@ -23,6 +25,14 @@ app.get('/', (request, response) => {
 
 app.get('/products', (request, response) => {
     response.json(products)
+})
+
+app.post('/products', (request, response) => {
+    console.log(request.body)
+
+    products.push(request.body)
+
+    response.sendStatus(201).json({ message: 'Product created successfully!'})
 })
 
 app.listen(3000, () => console.log('Server started on port 3000'))
